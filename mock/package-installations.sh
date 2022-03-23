d302a9f3207b3038ec71667e8a7f563f47832acd
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-03-23 16:28:20 -0600

    📅  | Wed Mar 23 16:28:20 MDT 2022

---
+++ mock/package-installations.sh
@@ -0,0 +1,6 @@
+ng add  @ngrx/store@latest
+ng add  @ngrx/effects@latest
+ng add  @ngrx/router-store@latest
+ng add  @ngrx/entity@latest
+ng add  @ngrx/store-devtools@latest
+ng add  @ngrx/schematics@latest
