e66271b5c7edf1a312bc23e9fdce24ea9cedc521
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-03-23 13:48:13 -0600

    📅  | Wed Mar 23 13:48:13 MDT 2022

--- mock/src/app/app.component.ts
+++ mock/src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'mock';
+  count: number = 0;
+
+  reset() {
+    this.count = 0;
+  }
+  decrement() {
+    this.count -= 1;
+  }
+  increment() {
+    this.count += 1;
+  }
 }
